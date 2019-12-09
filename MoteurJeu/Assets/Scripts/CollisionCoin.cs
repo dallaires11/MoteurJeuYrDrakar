0ec830ad9041f58ec3fc81e600064c286ad100a0
stevenduq <44072409+stevenduq@users.noreply.github.com>
2019-12-09 16:35:58 -0500

    tp5 fini

---
+++ MoteurJeu/Assets/Scripts/CollisionCoin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionCoin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    AudioSource audioSource;
+    public ScoreTracker scoreTracker;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag =="Player")
+        {
+            audioSource.Play();
+            StartCoroutine("Test");
+
+        }
+    }
+    IEnumerator Test()
+    {
+        print("Starting " + Time.time);
+
+        // Start function WaitAndPrint as a coroutine
+        //yield return StartCoroutine("WaitAndPrint");
+        scoreTracker.score += 1;
+        yield return new WaitForSeconds(0.6f);
+
+        Destroy(gameObject);
+        print("Done " + Time.time);
+    }
+}
---
+++ MoteurJeu/Assets/Scripts/RotationCollectible.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RotationCollectible : MonoBehaviour { 
+public Transform pivot; // le pivot (ici l'arme)
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        Vector3 targetP = pivot.position; // position du pivot
+        transform.RotateAround(targetP, Vector3.up, 50 * Time.deltaTime); // rotation du cylindre autour du pivot
+
+    }
+    
+}
